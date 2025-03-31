bf900a59189eda5259a30333b233a03d4c8aa5de
Kylen Williams <kylenwilliams99@gmail.com>
2025-03-31 13:36:07 -0400

    adds comments and prevents common name file recreation when it already exists, and whole-word enforcement

--- scraping_test/gbif_data_converter.py
+++ scraping_test/gbif_data_converter.py
@@ -1,35 +1,34 @@
+import csv
+import sys
+import os
+
 """
 This program takes the tsv of the common and scientific names gbif data, and removes everything
 that isn't just the name. This is so we can parse the data easier when checking for valid postings online.
 """
-import csv
-import sys
-import os
-import re
 
 csv.field_size_limit(sys.maxsize) # this is so we don't have problems reading from columns with lots of data
-    
+   
+# made this a function so that it can be applied to other lists of names with extraneous information
 def clean_data(input_filepath, output_filename, return_list=True):
     # opens the file at the given filepath, and then make a list of all the names from the list
-    with open(input_filepath) as fd:
-        rd = csv.reader(fd, delimiter="\t", quotechar='"')
-        next(rd)
+    with open(input_filepath) as f:
+        rd = csv.reader(f, delimiter="\t")
+        next(rd) # skips the title row
         common_names = [[row[1]] for row in rd]
-        # print(common_names)
     
     if not os.path.isdir("scraping_test/data"):
         os.makedirs("scraping_test/data")
 
-    # saves the names in a folder called data
+    # saves the common names in a folder called "data"
     output_filepath = "scraping_test/data/" + output_filename + ".csv"
     with open("scraping_test/data/common_names.csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=" ")
         writer.writerow(["Common Names"])
-        # writer.writerow([])
         writer.writerows(common_names)
-
-    if return_list:
-        # this turns each item in the list into a string pefore returning it
-        return list(map(str, common_names))
     
     print("CSV at " + output_filepath + " is complete!")
+
+    if return_list:
+        # this ensures each item in the list is a string before returning it
+        return list(map(str, common_names))
--- scraping_test/scraping.py
+++ scraping_test/scraping.py
@@ -6,17 +6,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import gbif_data_converter as dc
+import re
 
-# reminder to comment this code as it gets better and use black for formatting
+"""
+TODO:
+1. more efficnet name checking of description and title
+2. add in scrolling the page
+3. scrape the descriptions, images, and location data as well
+4. add in more craigslist regions (i.e links), and eventually more websites
+5. change to sets instead of lists for duplicate post prevention and faster operations, particualarly when scrolling
+6. make the filepaths windows compatible
+7. format with black
+"""
 
+# boilerplate code for initalizing the webdriver and connecting the code to chrome
 service = Service(executable_path="scraping_test/chromedriver-mac-arm64/chromedriver")
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # to keep the webpage from showing
+options.add_argument('--headless')  # keeps the webpage from showing
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://gainesville.craigslist.org/search/ccc#search=1~list~0~0")
 
-# we have to wait for the webpage to load
+# loads the webpage, and waits until either 10 seconds have passed or the posting titles have loaded
 try:
     WebDriverWait(driver, 10).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.cl-app-anchor.text-only.posting-title"))
@@ -26,31 +37,45 @@ except:
 
 titles = driver.find_elements(By.CSS_SELECTOR, "a.cl-app-anchor.text-only.posting-title")
 
-names = dc.clean_data("gbif_backbone/VernacularName.tsv", "common_names")
-animals = ["owl", "dog", "puppy", "puppies", "horse", "hound", "cat", "kitten", "rat", "pony", "dragon", "donkey", "pig", "bird", "pug", "pomeranian"]
-names.extend(animals)
+# calls gbif_data_converter to turn the gbif backbone into a csv of common names
+# eventually this will check if the CSV already exists before remaking it
+if not os.path.isfile("scraping_test/data/common_names.csv"):
+    names = dc.clean_data("gbif_backbone/VernacularName.tsv", "common_names")
+else: 
+    with open("scraping_test/data/common_names.csv") as f:
+        f = csv.reader(f)
+        next(f)
+        names = [str(name[0]).strip().lower() for name in f]
 
-matched_listings = []
+# poorly written, but it adds a few names I thought of that could be useful. Ideally we would add these to 
+# the actual csv. This will be deleted eventually
+extra_names = ["bushmeat", "endangered", "owl", "dog", "puppy", "puppies", "horse", "hound", "cat", "kitten", "rat", "pony", "dragon", "donkey", "pig", "bird", "pug", "pomeranian"]
+names.extend(extra_names)
 
+matched_listings = [] # an empty list to save the titles of posts we want to
 item_number = 1
-if titles:
-    for title in titles:
-        title = title.text.lower()
-        if any(name in title for name in names):
-            print(f"{item_number}: {title}")
-            matched_listings.append([title])
-            item_number+=1
-else:
-    print("no listings")
+# loops through the post titles we found and checks if they have any of our names in them
+for title in titles:
+    title_text = title.text.strip().lower()
+    
+    #the below is only comparing against extra_names because comparing against "names" in general is really 
+    # slow right now if you'd like to try, change "extra_names" in the following line to just "names"
+    if any(re.search(r'\b' + re.escape(name) + r'\b', title_text) for name in extra_names):  # this is a regular expressions (regex) pattern to ensure we only have whole names from
+                                                                                             # the names list. this may have a problem with two word animal names
+        print(f"{item_number}: {title_text}")
+        matched_listings.append([title_text])
+        item_number += 1
 
-driver.quit()
+driver.quit() # closes the webdriver
 
-# make this file pathing windows compatible later
+# makes a folder to save the data (if it doesn't already exist) and then saves the listings we want row by row
 if not os.path.isdir("scraping_test/data"):
     os.makedirs("scraping_test/data")
+    
 csv_filename = "scraping_test/data/filtered_listings.csv"
 with open(csv_filename, "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(["Matched Listings"])
     writer.writerow([])
     writer.writerows(matched_listings)
+
